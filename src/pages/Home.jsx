e6f09dbde6af3725de419b40d72ac1415f81aae6
Joey Pearson <jgpearson727@gmail.com>
2026-02-16 23:57:59 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -86,7 +86,7 @@ export default function Home() {
   };
 
   return (
-    <div className="min-h-screen bg-gradient-to-b from-orange-50 via-white to-teal-50 dark:from-gray-900 dark:via-gray-800 dark:to-gray-900">
+    <div className="min-h-screen min-h-[100dvh] bg-gradient-to-b from-orange-50 via-white to-teal-50 dark:from-gray-900 dark:via-gray-800 dark:to-gray-900">
       <OnboardingGuide open={showOnboarding} onClose={handleCloseOnboarding} />
       <ScanDetailModal 
         scan={selectedScan} 
