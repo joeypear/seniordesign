8ce04155aab17b397abfd9dde69950a40f40d2f2
Joey Pearson <jgpearson727@gmail.com>
2026-02-18 00:08:53 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -98,6 +98,7 @@ export default function Home() {
         onOpenChange={(open) => !open && setSelectedScan(null)}
         onUpdateNotes={(scanId, notes) => updateNotesMutation.mutateAsync({ scanId, notes })}
         onRenameScan={(scanId, name) => renameScanMutation.mutateAsync({ scanId, name })}
+        onDeleteScan={(scanId) => deleteScanMutation.mutateAsync(scanId)}
       />
       <div className="max-w-lg mx-auto px-4 py-8">
         {/* Header */}
