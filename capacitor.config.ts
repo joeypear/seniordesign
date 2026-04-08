868336dc896458653008d647070e7d66aef3ea33
jpear <85077738+joeypear@users.noreply.github.com>
2026-04-08 10:36:42 -0500

    updating

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.drmonster.app',
+  appName: 'DR Monster',
+  webDir: 'dist'
+};
+
+export default config;
